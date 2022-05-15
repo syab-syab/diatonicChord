fec105de804b9556243972c3b6303ace60161d63
syab-syab <syabsyabsyabsyab@gmail.com>
2022-05-15 22:46:51 +0900

    微修正

--- js/diatonic.js
+++ js/diatonic.js
@@ -1,7 +1,6 @@
-// ダイアトニックコードに使われるキーが
-// #の場合
+// #
 let $keyIndexSharp = ['A', 'A#', 'B','C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#',];
-// ♭の場合
+// ♭
 let $keyIndexFlat = ['A', 'B♭', 'B','C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭',];
 
 // キー選択のselectタグのid
@@ -10,18 +9,11 @@ let $selectKey = document.getElementById('choose-key');
 // ダイアトニックコードを表示するタグのid
 let $displaySpace = document.getElementById('display-space');
 
-// ♭と#で関数を分ける
-// 配列コピー → 選択キーの直前までの値を取り出してからケツにくっつける → htmlに描画する → 描画した要素が分かりやすいように背景色を変更するためにクラス追加
-
 // 選択されたキーが最初になるよう並び替える関数
 function rearrangeKey(arr, index) {
-  // 渡されたキーをコピー
   let arrKey = arr.concat();
-  // 渡されたインデックス数(セレクトされたキーのvalue)の直前までの値を取り出してからケツに付ける
   let splicesKey = arrKey.splice(0, parseInt(index));
   let concatsKey = arrKey.concat(splicesKey);
-  // あらかじめ表示する関数に渡す配列を作っておく
-  // インデックス数が 0, 2, 4, 5, 7, 9, 11
   let rearrangeArr = [];
   rearrangeArr.push(concatsKey[0] + "△7");
   rearrangeArr.push(concatsKey[2] + "m7");
@@ -35,11 +27,9 @@ function rearrangeKey(arr, index) {
 
 // ダイアトニックコードを表示
 function displayDiatonicChords(tag, arr) {
-  // tag.innerHTML = '<table border="1" bordercolor="#333333" cellspacing="0"><tr><th>Ⅰ△7</th><th>Ⅱm7</th><th>Ⅲm7</th><th>Ⅳ△7</th><th>Ⅴ7</th><th>Ⅵm7</th><th>Ⅶm7(♭5)</th></tr><tr><th>' + arr[0] + '△7</th><th>' + arr[2] + 'm7</th><th>' + arr[4] + 'm7</th><th>' + arr[5] + '△7</th><th>' + arr[7] + '7</th><th>' + arr[9] + 'm7</th><th>' + arr[11] + 'm7(♭5)</th></tr></table>';
   for (var i = 0; i < arr.length; i++) {
     document.getElementById("chord-" + i).innerHTML = arr[i]
   }
-  tag.classList.add("eme");
 }
 
 // キーが#
